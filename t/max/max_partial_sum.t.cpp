b6cc4b6edbe008a9fb784351d404293333551fb4
Todd Xue <xueweizhong@gmail.com>
2009-12-28 00:27:32 +0800

    add maximum partial sum algorithm testcases

---
+++ t/max/max_partial_sum.t.cpp
@@ -0,0 +1,113 @@
+#include "TAP.h"
+#include "max.h"
+#include <limits.h>
+#include <stdarg.h>
+
+int max_partial_sum_stupid(int n, int* a) {
+    int max = a[0];
+    int sum;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            // a[j] + ... a[i]
+            sum = 0;
+            for (int k = j; k <= i; ++k) sum += a[k];
+            if (sum > max)
+                max = sum;
+        }
+    }
+    return max;
+}
+
+bool random_check(int n, int max, int c) {
+    int* a = new int[n];
+    
+    bool ok = true;
+    for (int cc = 0; cc < c; ++cc) {
+        for (int i = 0; i < n; ++i) {
+            a[i] = random() % max; 
+            if (random() % 2) 
+                a[i] = -a[i];
+        }
+        int sum1 = max_partial_sum(n, a);
+        int sum2 = max_partial_sum_stupid(n, a);
+
+        if (sum1 != sum2) {
+            for (int i = 0; i < n; ++i) 
+                printf("%d ", a[i]);
+            printf("\nsum = %d, stupid=%d\n", sum1, sum2);
+            ok = false;
+            break;
+        }
+    }
+    delete[] a;
+    return ok;
+}
+
+static
+int my_max_partial_sum(int c, ...) {
+    int* a = new int[c];
+    va_list ap;
+    va_start(ap, c);
+    for (int i = 0; i < c; ++i)
+        a[i] = va_arg(ap, int);
+    int sum = max_partial_sum(c, a);
+    delete[] a;
+    return sum;
+}
+
+int main(int argc, char* argv[]) {
+    OK_SUM();
+
+    // 1 int
+    OK(my_max_partial_sum(1, 1) == 1);
+    
+    // 2 ints
+    OK(my_max_partial_sum(2, 1, 1) == 2);
+    OK(my_max_partial_sum(2, 1, -1) == 1);
+    OK(my_max_partial_sum(2, -1, 1) == 1);
+
+    // 3 ints
+    OK(my_max_partial_sum(3, 1, 1, 1) == 3);
+
+    OK(my_max_partial_sum(3, 1, -1, -1) == 1);
+    OK(my_max_partial_sum(3, -1, 1, -1) == 1);
+    OK(my_max_partial_sum(3, -1, -1, 1) == 1);
+
+    OK(my_max_partial_sum(3, 2, -1, -1) == 2);
+    OK(my_max_partial_sum(3, -1, 2, -1) == 2);
+    OK(my_max_partial_sum(3, -1, -1, 2) == 2);
+
+    // 4 ints
+    OK(my_max_partial_sum(4, 1, 1, -1, -1) == 2);
+    OK(my_max_partial_sum(4, 1, -1, 1, -1) == 1);
+    OK(my_max_partial_sum(4, 1, -1, -1, 1) == 1);
+
+    OK(my_max_partial_sum(4, -1, -1, 1, 1) == 2);
+    OK(my_max_partial_sum(4, -1, 1, -1, 1) == 1);
+    OK(my_max_partial_sum(4, -1, 1, 1, -1) == 2);
+
+    // 5 ints
+    OK(my_max_partial_sum(5, 1, 1, -1, -1, 5) == 5);
+    OK(my_max_partial_sum(5, 1, -1, 1, -1, 2) == 2);
+    OK(my_max_partial_sum(5, 1, -1, -1, 1, 1) == 2);
+
+    // random tests, maybe slow
+    OK(random_check(1, 10, 1000));
+
+    OK(random_check(2, 2, 1000));
+    OK(random_check(2, 4, 1000));
+
+    OK(random_check(4, 5, 10000));
+    OK(random_check(4, 10, 10000));
+
+    OK(random_check(20, 5, 10000));
+    OK(random_check(20, 10, 10000));
+
+    OK(random_check(30, 5, 10000));
+    OK(random_check(30, 10, 10000));
+
+    OK(random_check(40, 5, 10000));
+    OK(random_check(40, 10, 10000));
+
+    return 0;
+}
