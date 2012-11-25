cb57544da154b1b9c2d346ecc887d7f498227d49
Todd Xue <xueweizhong@gmail.com>
2012-11-25 11:13:36 -0800

    proection against loop inside rpermut

--- rpermut.cpp
+++ rpermut.cpp
@@ -1,4 +1,5 @@
 #include "rpermut.h"
+#include <stdio.h>
 
 bool rpermut_begin(int n, int* a, int m, int* vs)
 {
@@ -15,6 +16,15 @@ bool rpermut_next(int n, int* a, int m, int* vs)
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             if (a[i] == vs[j] && j+1 < m) {
+                
+                /**
+                 * protection of loop
+                 */
+                if (a[i] == vs[j+1]) {
+                    fprintf(stderr, "loop detected due to bad input\n");
+                    return false;
+                }
+
                 a[i] = vs[j+1];
                 for (int k = 0; k < i; ++k)
                     a[k] = vs[0];
